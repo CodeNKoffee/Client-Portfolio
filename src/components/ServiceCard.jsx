7669b78a73192f26c148d2ac8da1eee58c02fe7c
Hatem <hatemthedev@gmail.com>
2022-06-10 01:31:44 +0200

    Back side for ServiceCard components implemented

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -4,14 +4,20 @@ import RightArrowIcon from './assets/right_arrow.svg';
 const ServiceCard = (props) => {
   return (
     <div className="service">
-      <img src={props.icon} alt="Society" className="service__img" />
-      <h4 className="service__title">{props.title}</h4>
-      <button className="service__btn">
-        <h5 className="service__btn--title">
-          View More
-        </h5>
-        <img src={RightArrowIcon} alt="Right Arrow" className="service__btn--img" />
-      </button>
+      <div className="service__front">
+        <img src={props.icon} alt="Society" className="service__img" />
+        <h4 className="service__front--title">{props.title}</h4>
+        <button className="service__btn">
+          <h5 className="service__btn--title">
+            View More
+          </h5>
+          <img src={RightArrowIcon} alt="Right Arrow" className="service__btn--img" />
+        </button>
+      </div>
+      <div className="service__back">
+        <h4 className="service__back--title">I will:</h4>
+        <p className="service__back--para">{props.para}</p>
+      </div>
     </div>
   );
 }
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -18,10 +18,16 @@ const Services = () => {
             <ServiceCard
               icon={UsersIcon}
               title="Social Media Manager"
+              para="
+                create and maintain the company's brand with social media marketing tools, Interact with customers via the company's social media accounts, analyze the company's digital marketing plan and social media strategy and identify strategic weaknesses and making recommendations for improvements, and research social media trends and inform management of changes that are relevant to the company's marketing activities.
+              "
             />
             <ServiceCard
               icon={WizardHatIcon}
               title="Makeup Artist"
+              para="
+                communicate with clients to understand their needs and wants, determine necessary supplies and equipment needed, apply makeup in a professional manner, pay attention to other details such as costumes and design, examine makeup materials at disposal and chose the best option, prepare budget planning for makeup supplies and tools, keep makeup tools and products in check, adhere to personal hygiene standards, and stay up to date with the latest best practices, tools and technology in your field.
+              "
             />
           </div>
         </div>
