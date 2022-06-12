62c51a542f6cb515f6e9f7c0990d1721cf6a275e
Hatem <hatemthedev@gmail.com>
2022-06-12 17:40:39 +0200

    Service section 2/2 done

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -3,22 +3,25 @@ import RightArrowIcon from './assets/right_arrow.svg';
 
 const ServiceCard = (props) => {
   return (
-    <div className="service">
-      <div className="service__front">
-        <img src={props.icon} alt="Society" className="service__img" />
-        <h4 className="service__front--title">{props.title}</h4>
-        <button className="service__btn">
-          <h5 className="service__btn--title">
-            View More
-          </h5>
-          <img src={RightArrowIcon} alt="Right Arrow" className="service__btn--img" />
-        </button>
-      </div>
-      <div className="service__back">
-        <h4 className="service__back--title">I will:</h4>
-        <p className="service__back--para">{props.para}</p>
-      </div>
-    </div>
+    <label className="service">
+			<input className="checkbox" type="checkbox" />
+			<div class="service__card">
+				<div class="service__card--front">
+          <img src={props.icon} alt="Society" className="service__img" />
+          <h4 className="service__front--title">{props.title}</h4>
+          <button className="service__btn click">
+            <h5 className="service__btn--title">
+              View More
+            </h5>
+            <img src={RightArrowIcon} alt="Right Arrow" className="service__btn--img" />
+          </button>
+				</div>
+				<div class="service__card--back">
+          <h4 className="service__back--title">I will:</h4>
+          <p className="service__back--para">{props.para}</p>
+				</div>
+			</div>
+		</label>
   );
 }
 
