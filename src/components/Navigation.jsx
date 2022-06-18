1c725a19a8fbc642b7ed11a2a5b73b987c93a56f
Hatem <hatemthedev@gmail.com>
2022-06-18 20:43:04 +0200

    Navigation and Hero are responsive

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import NavLogo from './assets/nav_logo.svg'
 import BackgroundImg from './assets/smokie_bg.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 const Navigation = () => {
   return (
@@ -32,6 +35,30 @@ const Navigation = () => {
             </a>
           </li>
         </ul>
+        <div class="nav__container--mobile">
+          <button class="btn__menu" onclick="openMenu()">
+            <FontAwesomeIcon icon={faBars} />
+          </button>
+          <div class="menu__backdrop">
+            <button class="btn__menu btn__menu--close" onclick="closeMenu()">
+              <FontAwesomeIcon icon={faTimes} />
+            </button>
+            <ul class="menu__links">
+              <li class="menu__list">
+                <a class="menu__link" onclick="closeMenu()">ABOUT</a>
+              </li>
+              <li class="menu__list">
+                <a href="#explore" class="menu__link" onclick="closeMenu()">EXPLORE</a>
+              </li>
+              <li class="menu__list">
+                <a href="#journal" class="menu__link" onclick="closeMenu()">JOURNAL</a>
+              </li>
+              <li class="menu__list">
+                <a class="menu__link" onclick="closeMenu()">SEARCH</a>
+              </li>
+            </ul>
+          </div>
+        </div> 
       </nav>
     </div>
   );
